d3589761274b5002393e923a5b4347b99cf983c4
Lukas Potthast <privat@lukas-potthast.de>
2023-12-02 13:38:40 +0100

    Use std::any::type_name instead of custom name param

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,14 @@ mod day2;
 fn main() {
     util::tracing::init(LevelFilter::INFO);
 
-    run("day1::part1", day1::part1, 54601);
-    run("day1::part2", day1::part2, 54078);
-    run("day2::part1", day2::part1, 2317);
-    run("day2::part2", day2::part2, 74804);
+    run(day1::part1, 54601);
+    run(day1::part2, 54078);
+    run(day2::part1, 2317);
+    run(day2::part2, 74804);
 }
 
-#[tracing::instrument(level = "INFO", skip(fun, expected))]
-fn run<R: std::fmt::Debug + PartialEq>(name: &'static str, fun: impl Fn() -> R, expected: R) {
+#[tracing::instrument(level = "INFO", skip_all, fields(name = std::any::type_name::<F>()))]
+fn run<R: std::fmt::Debug + PartialEq, F: Fn() -> R>(fun: F, expected: R) {
     let result = fun();
     let avg = bench(fun);
     tracing::info!(avg = format!("{} μs", avg.as_micros()), ?result);
