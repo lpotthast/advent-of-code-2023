c879d977cec0db5b1499e10f4358fd0a304bfa16
Lukas Potthast <privat@lukas-potthast.de>
2023-12-03 17:20:13 +0100

    Only find parts if symbol is actually "gear"/'*'

--- src/day3.rs
+++ src/day3.rs
@@ -4,13 +4,17 @@ const INPUT: &str = include_str!("../res/day3.txt");
 
 #[tracing::instrument]
 pub fn part1() -> u64 {
-    Symbols::new(INPUT).map(|parts| parts.sum::<u64>()).sum()
+    Symbols::new(INPUT)
+        .map(|symbol| symbol.engine_parts().sum::<u64>())
+        .sum()
 }
 
 #[tracing::instrument]
 pub fn part2() -> u64 {
     Symbols::new(INPUT)
-        .filter(|parts| parts.symbol == '*' &&  parts.parts.iter().filter(|it| it.is_some()).count() == 2)
+        .filter(|symbol| symbol.symbol == '*')
+        .map(|symbol| symbol.engine_parts())
+        .filter(|parts| parts.len() == 2)
         .map(|mut parts| {
             let first = parts.next().unwrap();
             let second = parts.next().unwrap();
@@ -19,6 +23,21 @@ pub fn part2() -> u64 {
         .sum()
 }
 
+#[derive(Debug, Clone, Copy)]
+struct Symbol<'a> {
+    symbol: char,
+    symbol_idx: usize,
+
+    above: Option<&'a str>,
+    current: &'a str,
+    below: Option<&'a str>,
+}
+
+impl<'a> Symbol<'a> {
+    fn engine_parts(&self) -> EngineParts {
+        EngineParts::new(self.symbol_idx, self.above, self.current, self.below)
+    }
+}
 
 struct Symbols<'a> {
     line_iter: Lines<'a>,
@@ -67,7 +86,7 @@ impl<'a> Symbols<'a> {
 }
 
 impl<'a> Iterator for Symbols<'a> {
-    type Item = EngineParts;
+    type Item = Symbol<'a>;
 
     fn next(&mut self) -> Option<Self::Item> {
         self.current_symbol = None;
@@ -95,28 +114,36 @@ impl<'a> Iterator for Symbols<'a> {
         }
 
         match self.current_symbol {
-            Some(symbol) => Some(EngineParts::new(symbol, self.current_symbol_idx, self.above, self.current, self.below)),
+            Some(symbol) => Some(Symbol {
+                symbol,
+                symbol_idx: self.current_symbol_idx,
+                above: self.above,
+                current: self.current,
+                below: self.below,
+            }),
             None => None,
         }
     }
 }
 
-
 struct EngineParts {
-    pub(crate) symbol: char,
     pub(crate) parts: [Option<u64>; 6],
     next_part: usize,
 }
 
 impl EngineParts {
-    fn new<'a>(symbol: char, symbol_idx: usize, above: Option<&'a str>, current: &'a str, below: Option<&'a str>) -> Self {
+    fn new<'a>(
+        symbol_idx: usize,
+        above: Option<&'a str>,
+        current: &'a str,
+        below: Option<&'a str>,
+    ) -> Self {
         let (above_a, above_b) = Self::find_in_touching_line(above, symbol_idx);
         let left = Self::find_left(current, symbol_idx);
         let right = Self::find_right(current, symbol_idx);
         let (below_a, below_b) = Self::find_in_touching_line(below, symbol_idx);
 
         Self {
-            symbol,
             parts: [above_a, above_b, left, right, below_a, below_b],
             next_part: 0,
         }
@@ -145,11 +172,22 @@ impl EngineParts {
             .map(|_| read_num(current, symbol_idx + 1))
     }
 
-    fn find_in_touching_line(touching_line: Option<&str>, symbol_idx: usize) -> (Option<u64>, Option<u64>) {
+    fn find_in_touching_line(
+        touching_line: Option<&str>,
+        symbol_idx: usize,
+    ) -> (Option<u64>, Option<u64>) {
         match touching_line {
             Some(touching_line) => {
-                let left = if symbol_idx > 0 { symbol_idx - 1 } else { symbol_idx };
-                let right = if symbol_idx < touching_line.len() - 1 { symbol_idx + 1 } else { symbol_idx };
+                let left = if symbol_idx > 0 {
+                    symbol_idx - 1
+                } else {
+                    symbol_idx
+                };
+                let right = if symbol_idx < touching_line.len() - 1 {
+                    symbol_idx + 1
+                } else {
+                    symbol_idx
+                };
                 let mut chars_touching = touching_line[left..=right].chars();
 
                 let l = chars_touching.next();
@@ -157,20 +195,26 @@ impl EngineParts {
                 let r = chars_touching.next();
                 // tracing::info!(?l, ?m, ?r, "touching");
 
-                match m.filter(char::is_ascii_digit).map(|_m| read_num(touching_line, symbol_idx)) {
+                match m
+                    .filter(char::is_ascii_digit)
+                    .map(|_m| read_num(touching_line, symbol_idx))
+                {
                     Some(only_possible_num) => (Some(only_possible_num), None),
                     None => {
-                        let l = l.filter(char::is_ascii_digit).map(|_| read_num(touching_line, left));
-                        let r = r.filter(char::is_ascii_digit).map(|_| read_num(touching_line, right));
+                        let l = l
+                            .filter(char::is_ascii_digit)
+                            .map(|_| read_num(touching_line, left));
+                        let r = r
+                            .filter(char::is_ascii_digit)
+                            .map(|_| read_num(touching_line, right));
                         (l, r)
-                    },
+                    }
                 }
-            },
+            }
             None => (None, None),
         }
     }
 }
-
 
 impl Iterator for EngineParts {
     type Item = u64;
@@ -189,9 +233,30 @@ impl Iterator for EngineParts {
     }
 }
 
+impl ExactSizeIterator for EngineParts {
+    fn len(&self) -> usize {
+        self.parts.iter().filter(|it| it.is_some()).count()
+    }
+}
+
 fn read_num(input: &str, idx: usize) -> u64 {
-    let min_idx = idx - input[..idx].chars().rev().enumerate().skip_while(|(_i, c)| c.is_ascii_digit()).next().map(|(i, _c)| i).unwrap_or(idx);
-    let max_idx = idx + input[idx..].chars().enumerate().skip_while(|(_i, c)| c.is_ascii_digit()).next().map(|(i, _c)| i).unwrap_or(input.len() - idx);
+    let min_idx = idx
+        - input[..idx]
+            .chars()
+            .rev()
+            .enumerate()
+            .skip_while(|(_i, c)| c.is_ascii_digit())
+            .next()
+            .map(|(i, _c)| i)
+            .unwrap_or(idx);
+    let max_idx = idx
+        + input[idx..]
+            .chars()
+            .enumerate()
+            .skip_while(|(_i, c)| c.is_ascii_digit())
+            .next()
+            .map(|(i, _c)| i)
+            .unwrap_or(input.len() - idx);
     str::parse::<u64>(&input[min_idx..max_idx]).unwrap()
 }
 
