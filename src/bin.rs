1f57fe2ba0cd44f2908463f577946cb4bab7c54e
Lukas Potthast <privat@lukas-potthast.de>
2023-12-05 18:25:03 +0100

    Run test inputs on cargo run

--- src/bin.rs
+++ src/bin.rs
@@ -1,5 +1,12 @@
 use lib::days::*;
 
+const INPUT_D1_PART1_TEST: &str = include_str!("../res/day1_part1_test.txt");
+const INPUT_D1_PART2_TEST: &str = include_str!("../res/day1_part2_test.txt");
+const INPUT_D2_TEST: &str = include_str!("../res/day2_test.txt");
+const INPUT_D3_TEST: &str = include_str!("../res/day3_test.txt");
+const INPUT_D4_TEST: &str = include_str!("../res/day4_test.txt");
+const INPUT_D5_TEST: &str = include_str!("../res/day5_test.txt");
+
 const INPUT_D1: &str = include_str!("../res/day1.txt");
 const INPUT_D2: &str = include_str!("../res/day2.txt");
 const INPUT_D3: &str = include_str!("../res/day3.txt");
@@ -9,14 +16,28 @@ const INPUT_D5: &str = include_str!("../res/day5.txt");
 fn main() {
     init_tracing(tracing::level_filters::LevelFilter::INFO);
 
+    run(day1::part1, INPUT_D1_PART1_TEST, 142);
+    run(day1::part2, INPUT_D1_PART2_TEST, 281);
     run(day1::part1, INPUT_D1, 54_601);
     run(day1::part2, INPUT_D1, 54_078);
+
+    run(day2::part1, INPUT_D2_TEST, 8);
+    run(day2::part2, INPUT_D2_TEST, 2_286);
     run(day2::part1, INPUT_D2, 2_317);
     run(day2::part2, INPUT_D2, 74_804);
+
+    run(day3::part1, INPUT_D3_TEST, 4_361);
+    run(day3::part2, INPUT_D3_TEST, 467_835);
     run(day3::part1, INPUT_D3, 556_367);
     run(day3::part2, INPUT_D3, 89_471_771);
+
+    run(day4::part1, INPUT_D4_TEST, 13);
+    run(day4::part2, INPUT_D4_TEST, 230);
     run(day4::part1, INPUT_D4, 23_028);
     run(day4::part2, INPUT_D4, 9_236_992);
+
+    run(day5::part1, INPUT_D5_TEST, Some(35));
+    run(day5::part2, INPUT_D5_TEST, Some(46));
     run(day5::part1, INPUT_D5, Some(403_695_602));
     run(day5::part2, INPUT_D5, Some(219_529_182));
 }
