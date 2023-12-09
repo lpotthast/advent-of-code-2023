67c21a54c186fc04fe5df8337d772d947738eb7f
Lukas Potthast <privat@lukas-potthast.de>
2023-12-09 17:08:09 +0100

    Fix clippy

--- src/days/day9.rs
+++ src/days/day9.rs
@@ -5,7 +5,7 @@ pub fn part1(input: &str) -> i64 {
     input
         .lines()
         .map(|line| {
-            let mut history = SmallVec::<[i64; 32]>::from_iter(read_history(line));
+            let mut history: SmallVec<[i64; 32]> = read_history(line).collect();
             estimate_next(&mut history)
         })
         .sum()
@@ -15,7 +15,7 @@ pub fn part2(input: &str) -> i64 {
     input
         .lines()
         .map(|line| {
-            let mut history = SmallVec::<[i64; 32]>::from_iter(read_history(line));
+            let mut history: SmallVec<[i64; 32]> = read_history(line).collect();
             history.reverse();
             estimate_next(&mut history)
         })
@@ -26,7 +26,7 @@ fn estimate_next(hist: &mut [i64]) -> i64 {
     let mut next_len = hist.len() - 1;
     let mut slice = &mut hist[0..=next_len];
 
-    while slice.iter().find(|it| **it != 0).is_some() {
+    while slice.iter().any(|it| *it != 0) {
         for (i, j) in (0..slice.len()).tuple_windows::<(_, _)>() {
             slice[i] = slice[j] - slice[i];
         }
@@ -40,5 +40,5 @@ fn estimate_next(hist: &mut [i64]) -> i64 {
 fn read_history(input: &str) -> impl Iterator<Item = i64> + '_ {
     input
         .split_ascii_whitespace()
-        .map(|section| section.parse::<i64>().unwrap())
+        .map(|section| section.parse::<i64>().expect("number"))
 }
